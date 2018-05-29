93e35bcd6fa795a575f3a6af8af1c296dccf57cc
Aliaksandr Valialkin <valyala@gmail.com>
2018-05-29 16:14:39 +0300

    Added Scanner for parsing stream of JSON values

---
+++ scanner_example_test.go
@@ -0,0 +1,54 @@
+package fastjson_test
+
+import (
+	"fmt"
+	"github.com/valyala/fastjson"
+	"log"
+)
+
+func ExampleScanner() {
+	var sc fastjson.Scanner
+
+	sc.Init(`   {"foo":  "bar"  }[  ]
+		12345"xyz" true false null    `)
+
+	for sc.Next() {
+		fmt.Printf("%s\n", sc.Value())
+	}
+	if err := sc.Error(); err != nil {
+		log.Fatalf("unexpected error: %s", err)
+	}
+
+	// Output:
+	// {"foo":"bar"}
+	// []
+	// 12345
+	// "xyz"
+	// true
+	// false
+	// null
+}
+
+func ExampleScanner_reuse() {
+	var sc fastjson.Scanner
+
+	// The sc may be re-used in order to reduce the number
+	// of memory allocations.
+	for i := 0; i < 3; i++ {
+		s := fmt.Sprintf(`[%d] "%d"`, i, i)
+		sc.Init(s)
+		fmt.Printf("[")
+		for sc.Next() {
+			fmt.Printf("%s, ", sc.Value())
+		}
+		if err := sc.Error(); err != nil {
+			log.Fatalf("unexpected error: %s", err)
+		}
+		fmt.Printf("]\n")
+	}
+
+	// Output:
+	// [[0], "0", ]
+	// [[1], "1", ]
+	// [[2], "2", ]
+}
---
+++ scanner_test.go
@@ -0,0 +1,35 @@
+package fastjson
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestScanner(t *testing.T) {
+	var sc Scanner
+
+	t.Run("success", func(t *testing.T) {
+		sc.InitBytes([]byte(`[] {} "" 123`))
+		var bb bytes.Buffer
+		for sc.Next() {
+			v := sc.Value()
+			fmt.Fprintf(&bb, "%s", v)
+		}
+		if err := sc.Error(); err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		s := bb.String()
+		if s != `[]{}""123` {
+			t.Fatalf("unexpected string obtained; got %q; want %q", s, `[]{}""123`)
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		sc.Init(`[] sdfdsfdf`)
+		for sc.Next() {}
+		if err := sc.Error(); err == nil {
+			t.Fatalf("expecting non-nil error")
+		}
+	})
+}
