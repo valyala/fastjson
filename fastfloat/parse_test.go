bd9a6f14b378faad53dbad9704c9389ad05c9b68
Aliaksandr Valialkin <valyala@gmail.com>
2020-09-16 01:12:49 +0300

    fastfloat: add Parse(), ParseUint64() and ParseInt64() functions, which return parse errors

--- fastfloat/parse_test.go
+++ fastfloat/parse_test.go
@@ -13,7 +13,7 @@ func TestParseUint64BestEffort(t *testing.T) {
 
 		num := ParseUint64BestEffort(s)
 		if num != expectedNum {
-			t.Fatalf("unexpected umber parsed from %q; got %v; want %v", s, num, expectedNum)
+			t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
 		}
 	}
 
@@ -41,13 +41,64 @@ func TestParseUint64BestEffort(t *testing.T) {
 	f("18446744073709551616", 0)
 }
 
+func TestParseUint64Failure(t *testing.T) {
+	f := func(s string) {
+		t.Helper()
+
+		num, err := ParseUint64(s)
+		if err == nil {
+			t.Fatalf("expecting non-nil error")
+		}
+		if num != 0 {
+			t.Fatalf("unexpected number returned from ParseUint64(%q); got %v; want %v", s, num, 0)
+		}
+	}
+
+	// Invalid first char
+	f("")
+	f("   ")
+	f("foo")
+	f("-")
+	f("-foo")
+	f("-123")
+
+	// Invalid suffix
+	f("1foo")
+	f("13223 ")
+	f("1-2")
+
+	// Too big int
+	f("18446744073709551616")
+}
+
+func TestParseUint64Success(t *testing.T) {
+	f := func(s string, expectedNum uint64) {
+		t.Helper()
+
+		num, err := ParseUint64(s)
+		if err != nil {
+			t.Fatalf("unexpected error in ParseUint64(%q): %s", s, err)
+		}
+		if num != expectedNum {
+			t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
+		}
+	}
+
+	f("0", 0)
+	f("1", 1)
+	f("123", 123)
+	f("1234567890", 1234567890)
+	f("9223372036854775807", 9223372036854775807)
+	f("18446744073709551615", 18446744073709551615)
+}
+
 func TestParseInt64BestEffort(t *testing.T) {
 	f := func(s string, expectedNum int64) {
 		t.Helper()
 
 		num := ParseInt64BestEffort(s)
 		if num != expectedNum {
-			t.Fatalf("unexpected umber parsed from %q; got %v; want %v", s, num, expectedNum)
+			t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
 		}
 	}
 
@@ -76,6 +127,61 @@ func TestParseInt64BestEffort(t *testing.T) {
 	// Too big int
 	f("9223372036854775808", 0)
 	f("18446744073709551615", 0)
+}
+
+func TestParseInt64Failure(t *testing.T) {
+	f := func(s string) {
+		t.Helper()
+
+		num, err := ParseInt64(s)
+		if err == nil {
+			t.Fatalf("expecting non-nil error")
+		}
+		if num != 0 {
+			t.Fatalf("unexpected number returned from ParseInt64(%q); got %v; want %v", s, num, 0)
+		}
+	}
+
+	// Invalid first char
+	f("")
+	f("   ")
+	f("foo")
+	f("-")
+	f("-foo")
+
+	// Invalid suffix
+	f("1foo")
+	f("-13223 ")
+	f("1-2")
+
+	// Too big int
+	f("9223372036854775808")
+	f("18446744073709551615")
+	f("-18446744073709551615")
+}
+
+func TestParseInt64Success(t *testing.T) {
+	f := func(s string, expectedNum int64) {
+		t.Helper()
+
+		num, err := ParseInt64(s)
+		if err != nil {
+			t.Fatalf("unexpected error returned from ParseInt64(%q): %s", s, err)
+		}
+		if num != expectedNum {
+			t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
+		}
+	}
+
+	// Int
+	f("0", 0)
+	f("-0", 0)
+	f("1", 1)
+	f("123", 123)
+	f("-123", -123)
+	f("1234567890", 1234567890)
+	f("9223372036854775807", 9223372036854775807)
+	f("-9223372036854775807", -9223372036854775807)
 }
 
 func TestParseBestEffort(t *testing.T) {
@@ -126,6 +232,10 @@ func TestParseBestEffort(t *testing.T) {
 	f("123E-", 0)
 	f("123E+.", 0)
 	f("-123e-23foo", 0)
+	f("12345678901234567890foobar", 0)
+	f("0.12345678901234567890foobar", 0)
+	f("-0.12345678901234567890foobar", 0)
+	f("1e-400.5", 0)
 
 	// Integer
 	f("0", 0)
@@ -188,6 +298,157 @@ func TestParseBestEffort(t *testing.T) {
 	f("-1.E-10", -1.e-10)
 
 	// inf and nan
+	f("12345678909123456789012e45678", math.Inf(1))
+	f("-12345678909123456789012e45678", math.Inf(-1))
+	f("0.12345678909123456789012e45678", math.Inf(1))
+	f("-0.12345678909123456789012e45678", math.Inf(-1))
+	f("1.1e2345678909123456789012", math.Inf(1))
+	f("-1.1e2345678909123456789012", math.Inf(-1))
+	f("inf", math.Inf(1))
+	f("-Inf", math.Inf(-1))
+	f("+iNf", math.Inf(1))
+	f("INF", math.Inf(1))
+	f("nan", math.NaN())
+	f("-nan", math.NaN())
+	f("naN", math.NaN())
+	f("NaN", math.NaN())
+}
+
+func TestParseFailure(t *testing.T) {
+	f := func(s string) {
+		t.Helper()
+
+		num, err := Parse(s)
+		if err == nil {
+			t.Fatalf("expecting non-nil error")
+		}
+		if num != 0 {
+			t.Fatalf("unexpected number returned from ParseInt64(%q); got %v; want %v", s, num, 0)
+		}
+	}
+
+	// Invalid first char
+	f("")
+	f("  ")
+	f("foo")
+	f(" bar ")
+	f("-")
+	f("--")
+	f("-.")
+	f("-.e")
+	f("+112")
+	f("++")
+	f("e123")
+	f("E123")
+	f("-e12")
+	f(".")
+	f("..34")
+	f("-.32")
+	f("-.e3")
+	f(".e+3")
+
+	// Invalid suffix
+	f("1foo")
+	f("1  foo")
+	f("12.34.56")
+	f("13e34.56")
+	f("12.34e56e4")
+	f("12.")
+	f("123..45")
+	f("123ee34")
+	f("123e")
+	f("123e+")
+	f("123E-")
+	f("123E+.")
+	f("-123e-23foo")
+	f("12345678901234567890foobar")
+	f("0.12345678901234567890foobar")
+	f("-0.12345678901234567890foobar")
+	f("1e-400.5")
+}
+
+func TestParseSuccess(t *testing.T) {
+	f := func(s string, expectedNum float64) {
+		t.Helper()
+
+		num, err := Parse(s)
+		if err != nil {
+			t.Fatalf("unexpected error in Parse(%q): %s", s, err)
+		}
+		if math.IsNaN(expectedNum) {
+			if !math.IsNaN(num) {
+				t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
+			}
+		} else if num != expectedNum {
+			t.Fatalf("unexpected number parsed from %q; got %v; want %v", s, num, expectedNum)
+		}
+	}
+
+	// Integer
+	f("0", 0)
+	f("-0", 0)
+	f("0123", 123)
+	f("-00123", -123)
+	f("1", 1)
+	f("-1", -1)
+	f("1234567890123456", 1234567890123456)
+	f("12345678901234567", 12345678901234567)
+	f("123456789012345678", 123456789012345678)
+	f("1234567890123456789", 1234567890123456789)
+	f("12345678901234567890", 12345678901234567890)
+	f("-12345678901234567890", -12345678901234567890)
+	f("9223372036854775807", 9223372036854775807)
+	f("18446744073709551615", 18446744073709551615)
+	f("-18446744073709551615", -18446744073709551615)
+
+	// Fractional part
+	f("0.0", 0)
+	f("0.000", 0)
+	f("0.1", 0.1)
+	f("0.3", 0.3)
+	f("-0.1", -0.1)
+	f("-0.123", -0.123)
+	f("1.66", 1.66)
+	f("12345.12345678901", 12345.12345678901)
+	f("12345.123456789012", 12345.123456789012)
+	f("12345.1234567890123", 12345.1234567890123)
+	f("12345.12345678901234", 12345.12345678901234)
+	f("12345.123456789012345", 12345.123456789012345)
+	f("12345.1234567890123456", 12345.1234567890123456)
+	f("12345.12345678901234567", 12345.12345678901234567)
+	f("12345.123456789012345678", 12345.123456789012345678)
+	f("12345.1234567890123456789", 12345.1234567890123456789)
+	f("12345.12345678901234567890", 12345.12345678901234567890)
+	f("-12345.12345678901234567890", -12345.12345678901234567890)
+	f("18446744073709551615.18446744073709551615", 18446744073709551615.18446744073709551615)
+	f("-18446744073709551615.18446744073709551615", -18446744073709551615.18446744073709551615)
+
+	// Exponent part
+	f("0e0", 0)
+	f("123e+001", 123e1)
+	f("0e12", 0)
+	f("-0E123", 0)
+	f("-0E-123", 0)
+	f("-0E+123", 0)
+	f("123e12", 123e12)
+	f("-123E-12", -123e-12)
+	f("-123e-400", 0)
+	f("123e456", math.Inf(1))   // too big exponent
+	f("-123e456", math.Inf(-1)) // too big exponent
+	f("1e4", 1e4)
+	f("-1E-10", -1e-10)
+
+	// Fractional + exponent part
+	f("0.123e4", 0.123e4)
+	f("-123.456E-10", -123.456e-10)
+	f("1.e4", 1.e4)
+	f("-1.E-10", -1.e-10)
+
+	// inf and nan
+	f("12345678909123456789012e45678", math.Inf(1))
+	f("-12345678909123456789012e45678", math.Inf(-1))
+	f("0.12345678909123456789012e45678", math.Inf(1))
+	f("-0.12345678909123456789012e45678", math.Inf(-1))
 	f("inf", math.Inf(1))
 	f("-Inf", math.Inf(-1))
 	f("+iNf", math.Inf(1))
@@ -208,6 +469,25 @@ func TestParseBestEffortFuzz(t *testing.T) {
 			t.Fatalf("unexpected error when parsing %q: %s", s, err)
 		}
 		num := ParseBestEffort(s)
+		if num != numExpected {
+			t.Fatalf("unexpected number parsed from %q; got %g; want %g", s, num, numExpected)
+		}
+	}
+}
+
+func TestParseFuzz(t *testing.T) {
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < 100000; i++ {
+		f := r.Float64()
+		s := strconv.FormatFloat(f, 'g', -1, 64)
+		numExpected, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			t.Fatalf("unexpected error when parsing %q: %s", s, err)
+		}
+		num, err := Parse(s)
+		if err != nil {
+			t.Fatalf("unexpected error in Parse(%q): %s", s, err)
+		}
 		if num != numExpected {
 			t.Fatalf("unexpected number parsed from %q; got %g; want %g", s, num, numExpected)
 		}
--- fastfloat/parse_timing_test.go
+++ fastfloat/parse_timing_test.go
@@ -7,6 +7,30 @@ import (
 	"testing"
 )
 
+func BenchmarkParseUint64(b *testing.B) {
+	for _, s := range []string{"0", "12", "12345", "1234567890", "9223372036854775807"} {
+		b.Run(s, func(b *testing.B) {
+			benchmarkParseUint64(b, s)
+		})
+	}
+}
+
+func BenchmarkParseUint64BestEffort(b *testing.B) {
+	for _, s := range []string{"0", "12", "12345", "1234567890", "9223372036854775807"} {
+		b.Run(s, func(b *testing.B) {
+			benchmarkParseUint64BestEffort(b, s)
+		})
+	}
+}
+
+func BenchmarkParseInt64(b *testing.B) {
+	for _, s := range []string{"0", "12", "12345", "1234567890", "9223372036854775807"} {
+		b.Run(s, func(b *testing.B) {
+			benchmarkParseInt64(b, s)
+		})
+	}
+}
+
 func BenchmarkParseInt64BestEffort(b *testing.B) {
 	for _, s := range []string{"0", "12", "12345", "1234567890", "9223372036854775807"} {
 		b.Run(s, func(b *testing.B) {
@@ -21,6 +45,109 @@ func BenchmarkParseBestEffort(b *testing.B) {
 			benchmarkParseBestEffort(b, s)
 		})
 	}
+}
+
+func BenchmarkParse(b *testing.B) {
+	for _, s := range []string{"0", "12", "12345", "1234567890", "1234.45678", "1234e45", "12.34e-34", "12345.1234567890", "12345.12345678901"} {
+		b.Run(s, func(b *testing.B) {
+			benchmarkParse(b, s)
+		})
+	}
+}
+
+func benchmarkParseUint64(b *testing.B, s string) {
+	b.Run("std", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d uint64
+			for pb.Next() {
+				dd, err := strconv.ParseUint(s, 10, 64)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				d += dd
+			}
+			atomic.AddUint64(&Sink, d)
+		})
+	})
+	b.Run("custom", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d uint64
+			for pb.Next() {
+				dd, err := ParseUint64(s)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				d += dd
+			}
+			atomic.AddUint64(&Sink, d)
+		})
+	})
+}
+
+func benchmarkParseInt64(b *testing.B, s string) {
+	b.Run("std", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d int64
+			for pb.Next() {
+				dd, err := strconv.ParseInt(s, 10, 64)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				d += dd
+			}
+			atomic.AddUint64(&Sink, uint64(d))
+		})
+	})
+	b.Run("custom", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d int64
+			for pb.Next() {
+				dd, err := ParseInt64(s)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				d += dd
+			}
+			atomic.AddUint64(&Sink, uint64(d))
+		})
+	})
+}
+
+func benchmarkParseUint64BestEffort(b *testing.B, s string) {
+	b.Run("std", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d uint64
+			for pb.Next() {
+				dd, err := strconv.ParseUint(s, 10, 64)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				d += dd
+			}
+			atomic.AddUint64(&Sink, d)
+		})
+	})
+	b.Run("custom", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var d uint64
+			for pb.Next() {
+				d += ParseUint64BestEffort(s)
+			}
+			atomic.AddUint64(&Sink, d)
+		})
+	})
 }
 
 func benchmarkParseInt64BestEffort(b *testing.B, s string) {
@@ -48,6 +175,39 @@ func benchmarkParseInt64BestEffort(b *testing.B, s string) {
 				d += ParseInt64BestEffort(s)
 			}
 			atomic.AddUint64(&Sink, uint64(d))
+		})
+	})
+}
+
+func benchmarkParse(b *testing.B, s string) {
+	b.Run("std", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var f float64
+			for pb.Next() {
+				ff, err := strconv.ParseFloat(s, 64)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error: %s", err))
+				}
+				f += ff
+			}
+			atomic.AddUint64(&Sink, uint64(f))
+		})
+	})
+	b.Run("custom", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetBytes(int64(len(s)))
+		b.RunParallel(func(pb *testing.PB) {
+			var f float64
+			for pb.Next() {
+				ff, err := Parse(s)
+				if err != nil {
+					panic(fmt.Errorf("unexpected error in Parse(%q): %s", s, err))
+				}
+				f += ff
+			}
+			atomic.AddUint64(&Sink, uint64(f))
 		})
 	})
 }
