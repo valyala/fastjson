041656e2891ce723a68b8221237e8446a9dc91a5
Dylan Cant <dylanjessecant@gmail.com>
2023-01-18 00:40:20 -0500

    added \f \b according to RFC8259

--- parser.go
+++ parser.go
@@ -267,36 +267,40 @@ func parseObject(s string, c *cache, depth int) (*Value, string, error) {
 }
 
 func escapeString(dst []byte, s string) []byte {
-	dst = append(dst, 34)
+	dst = append(dst, '"')
 	for i := 0; i < len(s); i++ {
 		c := s[i]
 		switch {
-		case c == 0x22:
+		case c == '"':
 			// quotation mark
-			dst = append(dst, []byte{92, 34}...)
-		case c == 0x5c:
+			dst = append(dst, []byte{'\\', '"'}...)
+		case c == '\\':
 			// reverse solidus
-			dst = append(dst, []byte{92, 92}...)
+			dst = append(dst, []byte{'\\', '\\'}...)
 		case c >= 0x20:
-			// default, rest are control chars
+			// default, rest below are control chars
 			dst = append(dst, c)
+		case c == 0x08:
+			dst = append(dst, []byte{'\\', 'b'}...)
 		case c < 0x09:
-			dst = append(dst, []byte{92, 117, 48, 48, 48, 48 + c}...)
+			dst = append(dst, []byte{'\\', 'u', '0', '0', '0', '0' + c}...)
 		case c == 0x09:
-			dst = append(dst, []byte{92, 116}...)
+			dst = append(dst, []byte{'\\', 't'}...)
 		case c == 0x0a:
-			dst = append(dst, []byte{92, 110}...)
+			dst = append(dst, []byte{'\\', 'n'}...)
+		case c == 0x0c:
+			dst = append(dst, []byte{'\\', 'f'}...)
 		case c == 0x0d:
-			dst = append(dst, []byte{92, 114}...)
+			dst = append(dst, []byte{'\\', 'r'}...)
 		case c < 0x10:
-			dst = append(dst, []byte{92, 117, 48, 48, 48, 87 + c}...)
+			dst = append(dst, []byte{'\\', 'u', '0', '0', '0', 0x57 + c}...)
 		case c < 0x1a:
-			dst = append(dst, []byte{92, 117, 48, 48, 49, 32 + c}...)
+			dst = append(dst, []byte{'\\', 'u', '0', '0', '1', 0x20 + c}...)
 		case c < 0x20:
-			dst = append(dst, []byte{92, 117, 48, 48, 49, 71 + c}...)
+			dst = append(dst, []byte{'\\', 'u', '0', '0', '1', 0x47 + c}...)
 		}
 	}
-	dst = append(dst, 34)
+	dst = append(dst, '"')
 	return dst
 }
 
