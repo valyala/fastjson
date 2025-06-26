ae7cdc9eef1039a3857baff48c485190f2cf1acf
Tiago Peczenyj <tpeczenyj@weborama.com>
2025-06-26 14:24:19 +0200

    substitute b2s and s2b functions by same code from fasthttp v1.62.0

--- util.go
+++ util.go
@@ -1,23 +1,5 @@
 package fastjson
 
-import (
-	"reflect"
-	"unsafe"
-)
-
-func b2s(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
-func s2b(s string) (b []byte) {
-	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	sh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh.Data = strh.Data
-	sh.Len = strh.Len
-	sh.Cap = strh.Len
-	return b
-}
-
 const maxStartEndStringLen = 80
 
 func startEndString(s string) string {
